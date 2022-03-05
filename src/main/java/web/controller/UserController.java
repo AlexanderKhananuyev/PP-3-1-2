c77de6f07219bdcf2a8713f666dd4c3cdb02bf41
alexanderkhananuyev <amparlond@gmail.com>
2022-03-05 19:59:21 +0300

    init commit

---
+++ src/main/java/web/controller/UserController.java
@@ -0,0 +1,26 @@
+package web.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import web.service.UserService;
+
+import java.security.Principal;
+
+@Controller
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public String getUser (Model model, Principal principal) {
+        model.addAttribute("user",userService.getUserByName(principal.getName()));
+        return "user";
+    }
+}
